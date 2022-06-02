747d539c9152afc6c67c93ca8274175b97a0460a
Berkan Serbest <serbest@coffeeit.nl>
2022-06-02 10:00:04 +0200

    Sample app setup

---
+++ bottomsheet/src/main/java/nl/aroma/bottomsheet/BottomSheet.kt
@@ -0,0 +1,9 @@
+package nl.aroma.bottomsheet
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BottomSheetDialog() {
+    Text(text = "Hello Text compose")
+}
