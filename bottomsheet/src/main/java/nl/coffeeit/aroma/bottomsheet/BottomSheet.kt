2128a78756ac4a9715dfa23561f44f310e2de8c6
Armijn Vink <vink@coffeeit.nl>
2022-06-02 12:46:09 +0200

    Make runable

--- bottomsheet/src/main/java/nl/coffeeit/aroma/bottomsheet/BottomSheet.kt
+++ bottomsheet/src/main/java/nl/coffeeit/aroma/bottomsheet/BottomSheet.kt
@@ -1,6 +1,6 @@
-package nl.aroma.bottomsheet
+package nl.coffeeit.aroma.bottomsheet
 
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
--- sample/src/main/java/nl/coffeeit/aroma/sample/MainActivity.kt
+++ sample/src/main/java/nl/coffeeit/aroma/sample/MainActivity.kt
@@ -1,11 +1,17 @@
 package nl.coffeeit.aroma.sample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.compose.ui.platform.ComposeView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.action_active_development).setOnClickListener {
+            startActivity(Intent(this, ActiveDevelopmentActivity::class.java))
+        }
     }
 }
