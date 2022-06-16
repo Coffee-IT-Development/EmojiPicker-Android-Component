4bfe25d2e7ebf63eee564c2dc21a7da2272c2741
Daphne van der Veeke <daphnevdveeke@gmail.com>
2022-06-16 13:29:32 +0200

    Fix

--- gitrise.sh
+++ gitrise.sh
@@ -107,6 +107,10 @@ if [ "$DEBUG" == "true" ]; then
 fi
 
 function validate_input() {
+    printf "$WORKFLOW"
+    printf "$ACCESS_TOKEN"
+    printf "$PROJECT_SLUG"
+
     if [ -z "$WORKFLOW" ] || [ -z "$ACCESS_TOKEN" ] || [ -z "$PROJECT_SLUG" ]; then
         printf "\e[31m ERROR: Missing arguments(s). All these args must be passed: --workflow,--slug,--access-token \e[0m\n"
         usage
