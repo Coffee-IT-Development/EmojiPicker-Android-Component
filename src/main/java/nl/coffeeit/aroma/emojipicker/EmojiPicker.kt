4a067fdee4f44fb7401cfb7d8c8f5d5cebb91dde
Daphne van der Veeke <daphnevdveeke@gmail.com>
2022-06-17 13:50:14 +0200

    Initial commit

---
+++ src/main/java/nl/coffeeit/aroma/emojipicker/EmojiPicker.kt
@@ -0,0 +1,9 @@
+package nl.coffeeit.aroma.emojipicker
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EmojiPicker() {
+    Text(text = "Hello Text compose")
+}
---
+++ src/main/java/nl/coffeeit/aroma/emojipicker/domain/repository/EmojiRepository.kt
@@ -0,0 +1,22 @@
+package nl.coffeeit.aroma.emojipicker.domain.repository
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.internal.LinkedTreeMap
+import nl.coffeeit.aroma.emojipicker.R
+import nl.coffeeit.aroma.emojipicker.domain.model.EmojiItem
+import javax.inject.Singleton
+
+
+@Singleton
+class EmojiRepository {
+    private val emojis: Map<String, List<LinkedTreeMap<String, String>>>? = null
+    fun getEmojis(context: Context): Map<String, List<LinkedTreeMap<String, String>>>? {
+        if (emojis != null) return emojis
+        val json = context.resources.openRawResource(R.raw.emojis_by_group)
+            .bufferedReader().use { it.readText() }
+        var map: Map<String, List<LinkedTreeMap<String, String>>> = HashMap()
+        map = Gson().fromJson(json, map.javaClass)
+        return map
+    }
+}
