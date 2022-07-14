5c48a1f4e9b67d7f37f387b547ae6571b4681580
Daphne van der Veeke <daphnevdveeke@gmail.com>
2022-07-14 15:14:23 +0200

    WIP Pincode

--- sample/src/main/java/nl/coffeeit/aroma/sample/presentation/pincode/PincodeActivity.kt
+++ sample/src/main/java/nl/coffeeit/aroma/sample/presentation/pincode/PincodeActivity.kt
@@ -5,11 +5,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
@@ -17,6 +16,8 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nl.coffeeit.aroma.pincode.presentation.PincodeView
@@ -44,76 +45,259 @@ fun PincodeScreen() {
         }
     ) {
 
-        Column {
+        Column(
+            Modifier.verticalScroll(rememberScrollState())
+        ) {
             Column(
                 Modifier
                     .background(Color(0xFF332940))
-                    .padding(16.dp)
+                    .padding(vertical = 32.dp)
             ) {
                 Text(
                     text = "SocialBlox",
-                    color = Color.White
+                    color = Color.White,
+                    modifier = Modifier.padding(horizontal = 16.dp)
                 )
 
-                Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(32.dp))
 
                 PincodeView(
-                    6,
+                    lengthOfCode = 6,
                     inputCornerShape = RoundedCornerShape(8.dp),
                     inputColors = TextFieldDefaults.outlinedTextFieldColors(
                         focusedBorderColor = Color(0xFF87888A),
                         unfocusedBorderColor = Color(0xFF36343D),
-                        backgroundColor = Color(0xFF36343D)
+                        backgroundColor = Color(0xFF36343D),
+                        textColor = Color.White,
+                        cursorColor = Color.Transparent,
+                        errorCursorColor = Color.Transparent
                     ),
-                    inputSpacing = 16.dp
+                    inputSpacing = 16.dp,
+                    modifier = Modifier.padding(horizontal = 27.dp),
+                    inputTextStyle = TextStyle(
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                PincodeView(
+                    lengthOfCode = 6,
+                    inputCornerShape = RoundedCornerShape(8.dp),
+                    inputColors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFF87888A),
+                        unfocusedBorderColor = Color(0xFF36343D),
+                        backgroundColor = Color(0xFF36343D),
+                        errorBorderColor = Color(0xFF36343D),
+                        cursorColor = Color.Transparent,
+                        errorCursorColor = Color.Transparent
+                    ),
+                    inputSpacing = 16.dp,
+                    modifier = Modifier.padding(horizontal = 27.dp),
+                    isError = true,
+                    errorText = "An error occurred",
+                    inputTextStyle = TextStyle(
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    ),
+                    inputErrorTextStyle = TextStyle(
+                        textAlign = TextAlign.Center,
+                        color = Color(0xFFF7694A)
+                    ),
+                    errorLabelTextStyle = TextStyle(
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    )
+                )
+            }
+
+            Column(
+                Modifier
+                    .background(Color.White)
+                    .padding(vertical = 32.dp)
+            ) {
+                Text(
+                    text = "In-lite",
+                    color = Color(0xFF2B3C46),
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                PincodeView(
+                    lengthOfCode = 6,
+                    inputCornerShape = RoundedCornerShape(6.dp),
+                    inputColors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFFF5F5F5),
+                        unfocusedBorderColor = Color(0xFFF5F5F5),
+                        backgroundColor = Color(0xFFF5F5F5),
+                        cursorColor = Color.Transparent,
+                        errorCursorColor = Color.Transparent
+                    ),
+                    inputSpacing = 8.dp,
+                    modifier = Modifier.padding(horizontal = 30.dp),
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                PincodeView(
+                    lengthOfCode = 6,
+                    inputCornerShape = RoundedCornerShape(6.dp),
+                    inputColors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFFF5F5F5),
+                        unfocusedBorderColor = Color(0xFFF5F5F5),
+                        backgroundColor = Color(0xFFF5F5F5),
+                        errorLabelColor = Color(0xFFF7694A),
+                        errorBorderColor = Color(0xFFF7694A),
+                        cursorColor = Color.Transparent,
+                        errorCursorColor = Color.Transparent
+                    ),
+                    inputSpacing = 8.dp,
+                    modifier = Modifier.padding(horizontal = 30.dp),
+                    isError = true
                 )
             }
 
             Column(
                 Modifier
                     .background(Color(0xFF8058D4))
-                    .padding(16.dp)
+                    .padding(vertical = 32.dp)
             ) {
                 Text(
                     text = "Babymanager",
-                    color = Color.White
+                    color = Color.White,
+                    modifier = Modifier.padding(horizontal = 16.dp)
                 )
 
-                Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(32.dp))
 
                 PincodeView(
-                    6,
+                    lengthOfCode = 5,
                     inputCornerShape = RoundedCornerShape(8.dp),
                     inputColors = TextFieldDefaults.outlinedTextFieldColors(
                         focusedBorderColor = Color.White,
                         unfocusedBorderColor = Color.White,
                         backgroundColor = Color.White,
+                        cursorColor = Color.Transparent,
+                        errorCursorColor = Color.Transparent
                     ),
-                    inputSpacing = 13.75.dp
+                    inputSpacing = 13.75.dp,
+                    modifier = Modifier.padding(horizontal = 40.dp)
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                PincodeView(
+                    lengthOfCode = 5,
+                    inputCornerShape = RoundedCornerShape(8.dp),
+                    inputColors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.White,
+                        backgroundColor = Color.White,
+                        cursorColor = Color.Transparent,
+                        errorCursorColor = Color.Transparent
+                    ),
+                    inputSpacing = 13.75.dp,
+                    modifier = Modifier.padding(horizontal = 40.dp),
+                    isError = true
                 )
             }
 
             Column(
                 Modifier
-                    .background(Color(0xFFFF00FF))
-                    .padding(16.dp)
+                    .background(Color.White)
+                    .padding(vertical = 32.dp)
             ) {
                 Text(
-                    text = "Random",
-                    color = Color.White
+                    text = "Bagtag",
+                    color = Color(0xFF1F325A),
+                    modifier = Modifier.padding(horizontal = 16.dp)
                 )
 
-                Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(32.dp))
 
                 PincodeView(
-                    8,
-                    inputCornerShape = RoundedCornerShape(0.dp),
+                    lengthOfCode = 6,
+                    inputCornerShape = RoundedCornerShape(8.dp),
                     inputColors = TextFieldDefaults.outlinedTextFieldColors(
-                        focusedBorderColor = Color.Black,
-                        unfocusedBorderColor = Color.White,
-                        backgroundColor = Color.Cyan,
+                        focusedBorderColor = Color(0xFFC5CDD9),
+                        unfocusedBorderColor = Color(0xFFF5F5F5),
+                        backgroundColor = Color(0xFFF5F5F5),
+                        cursorColor = Color.Transparent,
+                        errorCursorColor = Color.Transparent
                     ),
-                    inputSpacing = 8.dp
+                    inputSpacing = 8.dp,
+                    showDividerAfterInput = 3,
+                    dividerColor = Color(0xFF1F325A),
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                PincodeView(
+                    lengthOfCode = 6,
+                    inputCornerShape = RoundedCornerShape(8.dp),
+                    inputColors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFFC5CDD9),
+                        unfocusedBorderColor = Color(0xFFF5F5F5),
+                        backgroundColor = Color(0xFFF5F5F5),
+                        cursorColor = Color.Transparent,
+                        errorCursorColor = Color.Transparent
+                    ),
+                    inputSpacing = 8.dp,
+                    showDividerAfterInput = 3,
+                    dividerColor = Color(0xFF1F325A),
+                    modifier = Modifier.padding(horizontal = 24.dp),
+                    isError = true,
+                    errorText = "An error occurred"
+                )
+            }
+
+            Column(
+                Modifier
+                    .background(Color(0xFFF8FBFF))
+                    .padding(vertical = 32.dp)
+            ) {
+                Text(
+                    text = "Workstead",
+                    color = Color(0xFF195393),
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                PincodeView(
+                    lengthOfCode = 5,
+                    inputWidth = 40.dp,
+                    inputCornerShape = RoundedCornerShape(8.dp),
+                    inputColors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFFA3BAD4),
+                        unfocusedBorderColor = Color(0xFFA3BAD4),
+                        backgroundColor = Color.White,
+                        cursorColor = Color.Transparent,
+                        errorCursorColor = Color.Transparent
+                    ),
+                    inputSpacing = 12.dp,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                PincodeView(
+                    lengthOfCode = 5,
+                    inputWidth = 40.dp,
+                    inputCornerShape = RoundedCornerShape(8.dp),
+                    inputColors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFFA3BAD4),
+                        unfocusedBorderColor = Color(0xFFA3BAD4),
+                        backgroundColor = Color.White,
+                        cursorColor = Color.Transparent,
+                        errorCursorColor = Color.Transparent
+                    ),
+                    inputSpacing = 12.dp,
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    isError = true
                 )
             }
         }
